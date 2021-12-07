075499ee12a55e0ac27f137aff1ba18d5b21cd82
msxingpeng <xingpeng@microsoft.com>
2021-12-07 01:10:33 +0000

    adding testing

--- tests/test_create_dataset_manifest.py
+++ tests/test_create_dataset_manifest.py
@@ -300,6 +300,48 @@ class TestCreateCocoDatasetManifest(unittest.TestCase):
             self.assertEqual(dataset_manifest.images[0].labels, [[0, 10, 10, 100, 100]])
             self.assertEqual(dataset_manifest.images[1].labels, [[0, 100, 100, 200, 200], [1, 20, 20, 200, 200]])
 
+    def test_image_caption(self):
+        manifest_dict = {
+            "images": [{"id": 1, "file_name": "train_images.zip@honda.jpg"},
+                       {"id": 2, "file_name": "train_images.zip@kitchen.jpg"}],
+            "annotations": [
+                {"id": 1, "image_id": 1, "caption": "A black Honda motorcycle parked in front of a garage."},
+                {"id": 2, "image_id": 1, "caption": "A Honda motorcycle parked in a grass driveway."},
+                {"id": 3, "image_id": 1, "caption": "A black Honda motorcycle with a dark burgundy seat."},
+                {"id": 4, "image_id": 1, "caption": "Ma motorcycle parked on the gravel in front of a garage."},
+                {"id": 5, "image_id": 1, "caption": "A motorcycle with its brake extended standing outside."},
+                {"id": 6, "image_id": 2, "caption": "A picture of a modern looking kitchen area.\n"},
+                {"id": 7, "image_id": 2, "caption": "A narrow kitchen ending with a chrome refrigerator."},
+                {"id": 8, "image_id": 2, "caption": "A narrow kitchen is decorated in shades of white, gray, and black."},
+                {"id": 9, "image_id": 2, "caption": "a room that has a stove and a icebox in it"},
+                {"id": 10, "image_id": 2, "caption": "A long empty, minimal modern skylit home kitchen."}
+            ],
+            "type": "captions"
+        }
+        labels_image_1 = ['A black Honda motorcycle parked in front of a garage.',
+                          'A Honda motorcycle parked in a grass driveway.',
+                          'A black Honda motorcycle with a dark burgundy seat.',
+                          'Ma motorcycle parked on the gravel in front of a garage.',
+                          'A motorcycle with its brake extended standing outside.']
+        labels_image_2 = ['A picture of a modern looking kitchen area.\n',
+                          'A narrow kitchen ending with a chrome refrigerator.',
+                          'A narrow kitchen is decorated in shades of white, gray, and black.',
+                          'a room that has a stove and a icebox in it',
+                          'A long empty, minimal modern skylit home kitchen.']
+        dataset_dict = copy.deepcopy(self.DATASET_INFO_DICT)
+        with tempfile.TemporaryDirectory() as tempdir:
+            dataset_dict['root_folder'] = ''
+            dataset_dict['type'] = 'image_caption'
+            coco_file_path = os.path.join(tempdir, 'test.json')
+            with open(coco_file_path, 'w') as f:
+                json.dump(manifest_dict, f)
+
+            dataset_manifest = CocoManifestAdaptor.create_dataset_manifest(coco_file_path, DatasetTypes.IMCAP)
+            self.assertIsInstance(dataset_manifest, DatasetManifest)
+            self.assertEqual(len(dataset_manifest.images), 2)
+            self.assertEqual(dataset_manifest.images[0].labels, labels_image_1)
+            self.assertEqual(dataset_manifest.images[1].labels, labels_image_2)
+
 
 class TestManifestFewShotSample(unittest.TestCase):
     def test_multiclass_sample_10_out_of_30(self):
