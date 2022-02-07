716bbe4b97827aeccdedd7dbceaf69783b841657
TK <pinjin@microsoft.com>
2022-02-07 09:49:36 -0800

    Powerful dataset manifest merge; can get datasets of multiple usages (#22)

--- vision_datasets/resources/dataset_hub.py
+++ vision_datasets/resources/dataset_hub.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Union
+from typing import Union, List
 
 from ..common import DatasetRegistry, Usages
 from ..common.data_manifest import DatasetManifest
@@ -39,7 +39,7 @@ class DatasetHub(object):
             local_dir: local directory where datasets can be found/downloaded to
             name: dataset name
             version: dataset version, if not specified, latest version will be returned
-            usage: usage of the dataset, 'train', 'val' or 'test'
+            usage: usage(s) of the dataset, 'train', 'val' or 'test' or a list of usages
             coordinates: format of the bounding boxes, can be 'relative' or 'absolute'
             few_shot_samples_per_class (int): get a sampled dataset with N images at most for each class (for detection and multilabel case, not guaranteed.)
             rnd_seed (int): random seed for few shot sampling
@@ -56,7 +56,7 @@ class DatasetHub(object):
         from vision_datasets import ManifestDataset
         return ManifestDataset(dataset_info, manifest, coordinates, downloader_resources)
 
-    def create_dataset_manifest(self, container_sas: str, local_dir: str, name: str, version: int = None, usage: str = Usages.TRAIN_PURPOSE, few_shot_samples_per_class=None, rnd_seed=0):
+    def create_dataset_manifest(self, container_sas: str, local_dir: str, name: str, version: int = None, usage: Union[str, List] = Usages.TRAIN_PURPOSE, few_shot_samples_per_class=None, rnd_seed=0):
         """Create dataset manifest.
             If local_dir is provided, manifest_dataset consumes data from local disk. If data not present on local disk, it will be automatically downloaded.
             if container_sas is provided but local_dir not provided, manifest_dataset consumes data directly from container_sas.
@@ -69,18 +69,23 @@ class DatasetHub(object):
             local_dir: local directory where datasets can be found/downloaded to
             name: dataset name
             version: dataset version, if not specified, latest version will be returned
-            usage: usage of the dataset, 'train', 'val' or 'test'
+            usage: usage(s) of the dataset, 'train', 'val' or 'test' or a list of usages
             few_shot_samples_per_class (int): get a sampled dataset with N images at most for each class (for detection and multilabel case, not guaranteed.)
             rnd_seed (int): random seed for few shot sampling
 
         Returns:
-            dataset manifest dataset, dataset_info, downloaded_resources, if dataset exists, else None
+            dataset manifest, dataset_info, downloaded_resources, if dataset exists, else None
         """
         assert container_sas or local_dir
         assert name
+        assert usage
 
         from ..common.dataset_downloader import DatasetDownloader
 
+        usages = usage if isinstance(usage, list) else [usage]
+
+        manifest = None
+        downloader_resources = None
         dataset_info = self.dataset_registry.get_dataset_info(name, version)
         if dataset_info is None:
             logger.warning(f'Dataset with {name} and version {version} not found.')
@@ -88,19 +93,26 @@ class DatasetHub(object):
 
         if isinstance(dataset_info, MultiTaskDatasetInfo):
             for task_info in dataset_info.sub_task_infos.values():
-                task_info.index_files = {usage: task_info.index_files[usage]} if usage in task_info.index_files else {}
+                task_info.index_files = {usage: task_info.index_files[usage] for usage in usages if usage in task_info.index_files}
         else:
-            dataset_info.index_files = {usage: dataset_info.index_files[usage]} if usage in dataset_info.index_files else {}
+            dataset_info.index_files = {usage: dataset_info.index_files[usage] for usage in usages if usage in dataset_info.index_files}
 
         if container_sas and local_dir:
             downloader = DatasetDownloader(container_sas, self.dataset_registry)
-            downloader_resources = downloader.download(name, version, local_dir, [usage])
+            downloader_resources_usage = downloader.download(name, version, local_dir, usages)
         else:
-            downloader_resources = None
+            downloader_resources_usage = None
 
-        manifest = DatasetManifest.create_dataset_manifest(dataset_info, usage, local_dir or container_sas)
+        for usage in usages:
+            manifest_usage = DatasetManifest.create_dataset_manifest(dataset_info, usage, local_dir or container_sas)
+            if manifest_usage:
+                manifest = DatasetManifest.merge(manifest, manifest_usage) if manifest else manifest_usage
 
-        if not manifest:
+            if downloader_resources_usage:
+                from ..common.dataset_downloader import DownloadedDatasetsResources
+                downloader_resources = DownloadedDatasetsResources.merge(downloader_resources, downloader_resources_usage) if downloader_resources else downloader_resources_usage
+
+        if manifest is None:
             return None
 
         if few_shot_samples_per_class:
