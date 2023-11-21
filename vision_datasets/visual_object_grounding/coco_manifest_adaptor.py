d2d45221ce76b46771c1b045fe862beb08afd197
shono <>
2023-11-21 11:57:55 -0800

    Object Grounding: Use LTWH coordinates by default

--- vision_datasets/visual_object_grounding/coco_manifest_adaptor.py
+++ vision_datasets/visual_object_grounding/coco_manifest_adaptor.py
@@ -1,4 +1,5 @@
-from ..common import DatasetTypes, ImageDataManifest, CocoManifestWithoutCategoriesAdaptor, CocoManifestAdaptorFactory
+import copy
+from ..common import BBoxFormat, DatasetTypes, ImageDataManifest, CocoManifestWithoutCategoriesAdaptor, CocoManifestAdaptorFactory
 from .manifest import VisualObjectGroundingLabelManifest
 
 
@@ -8,5 +9,14 @@ class VisualObjectGroundingCocoManifestAdaptor(CocoManifestWithoutCategoriesAdap
         super().__init__(DatasetTypes.VISUAL_OBJECT_GROUNDING)
 
     def process_label(self, image: ImageDataManifest, annotation: dict, coco_manifest: dict):
-        image.labels.append(VisualObjectGroundingLabelManifest({'question': annotation['question'], 'answer': annotation['answer'], 'groundings': annotation['groundings']},
+        bbox_format = coco_manifest.get('bbox_format')
+        bbox_format = BBoxFormat[bbox_format.upper()] if bbox_format else BBoxFormat.LTWH
+
+        groundings = copy.deepcopy(annotation['groundings'])
+        if bbox_format != BBoxFormat.LTRB:
+            for grounding in groundings:
+                boxes = grounding['bboxes']
+                grounding['bboxes'] = [[box[0], box[1], box[0] + box[2], box[1] + box[3]] for box in boxes]
+
+        image.labels.append(VisualObjectGroundingLabelManifest({'question': annotation['question'], 'answer': annotation['answer'], 'groundings': groundings},
                                                                additional_info=self._get_additional_info(annotation, {'id', 'question', 'answer', 'groundings'})))
