812bc427e30daba9ad05adf27a04da9780d802e5
Nishant Yadav <nishantyadav@microsoft.com>
2024-08-30 13:07:05 -0700

    minor cleanup

--- tests/test_ic_od_to_kvp_wrapper/test_classification_as_kvp.py
+++ tests/test_ic_od_to_kvp_wrapper/test_classification_as_kvp.py
@@ -15,6 +15,7 @@ from vision_datasets.common import (
 from vision_datasets.image_classification.classification_as_kvp_dataset import (
     ClassificationAsKeyValuePairDataset,
 )
+from vision_datasets.key_value_pair.manifest import KeyValuePairLabelManifest
 
 
 class TestMultilcassClassificationDataset:
@@ -60,12 +61,9 @@ class TestMultilcassClassificationDataset:
 
 
 class TestClassificationAsKeyValuePairDataset(unittest.TestCase):
-    def test_simple(self):
+    def test_multiclass_classification(self):
         sample_classification_dataset, _ = TestMultilcassClassificationDataset.create_an_ic_dataset()
         kvp_dataset = ClassificationAsKeyValuePairDataset(sample_classification_dataset)
-
-        print(kvp_dataset)
-
         self.assertIsInstance(kvp_dataset, ClassificationAsKeyValuePairDataset)
         self.assertEqual(kvp_dataset.dataset_info.type, DatasetTypes.KEY_VALUE_PAIR)
         self.assertIn("name", kvp_dataset.dataset_info.schema)
@@ -83,6 +81,9 @@ class TestClassificationAsKeyValuePairDataset(unittest.TestCase):
                              }
                          }
         })
+
+        _, target, _ = kvp_dataset[0]
+        self.assertIsInstance(target, KeyValuePairLabelManifest)
 
 
 if __name__ == '__main__':
