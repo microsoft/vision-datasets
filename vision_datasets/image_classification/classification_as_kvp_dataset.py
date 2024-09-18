65fa6e9fb4fb884a7b90c97b7aea110c641de564
chongyangbai <chongyangbai@microsoft.com>
2024-09-18 11:54:05 -0700

    init commit for multilabel-ic to kvp

--- vision_datasets/image_classification/classification_as_kvp_dataset.py
+++ vision_datasets/image_classification/classification_as_kvp_dataset.py
@@ -11,21 +11,7 @@ from vision_datasets.key_value_pair import (
 logger = logging.getLogger(__name__)
 
 
-CLASS_NAME_KEY = "className"
-BASE_CLASSIFICATION_SCHEMA = {
-    "name": "Multiclass image classification",
-    "description": "Classify images into one of the provided classes.",
-    "fieldSchema": {
-            f"{CLASS_NAME_KEY}": {
-                "type": "string",
-                "description": "Class name that the image belongs to.",
-                "classes": {}
-            }
-    }
-}
-
-
-class ClassificationAsKeyValuePairDataset(VisionDataset):
+class ClassificationAsKeyValuePairDatasetBase(VisionDataset):
     """Dataset class that access Classification datset as KeyValuePair dataset."""
 
     def __init__(self, classification_dataset: VisionDataset):
@@ -35,8 +21,7 @@ class ClassificationAsKeyValuePairDataset(VisionDataset):
             classification_dataset (VisionDataset): The classification dataset to convert to key-value pair dataset.
         """
 
-        if classification_dataset is None or classification_dataset.dataset_info.type not in {DatasetTypes.IMAGE_CLASSIFICATION_MULTICLASS}:
-            # TODO: Add support for multilabel classification
+        if classification_dataset is None or classification_dataset.dataset_info.type not in {DatasetTypes.IMAGE_CLASSIFICATION_MULTICLASS, DatasetTypes.IMAGE_CLASSIFICATION_MULTILABEL}:
             raise ValueError
 
         # Generate schema and update dataset info
@@ -55,10 +40,10 @@ class ClassificationAsKeyValuePairDataset(VisionDataset):
         # Construct KeyValuePairDatasetManifest
         annotations = []
         for id, img in enumerate(classification_dataset.dataset_manifest.images, 1):
-            label_id = img.labels[0].label_data
-            label_name = self.class_id_to_names[label_id]
+            label_ids = [l.label_data for l in img.labels]
+            label_names = [self.class_id_to_names[id] for id in label_ids]
 
-            kvp_label_data = self.construct_kvp_label_data(label_name)
+            kvp_label_data = self.construct_kvp_label_data(label_names)
             img_ids = [self.img_id_to_pos[img.id]]  # 0-based index
             kvp_annotation = KeyValuePairLabelManifest(id, img_ids, label_data=kvp_label_data)
 
@@ -70,20 +55,79 @@ class ClassificationAsKeyValuePairDataset(VisionDataset):
         super().__init__(dataset_info, dataset_manifest, dataset_resources=classification_dataset.dataset_resources)
 
     def construct_schema(self, class_names: typing.List[str]) -> typing.Dict[str, typing.Any]:
-        schema: typing.Dict[str, typing.Any] = BASE_CLASSIFICATION_SCHEMA  # initialize with base schema
-        schema["fieldSchema"][f"{CLASS_NAME_KEY}"]["classes"] = {c: {"description": f"A single class name. Only output {c} as the class name if present."} for c in class_names}
+        raise NotImplementedError
+
+    def construct_kvp_label_data(self, label_names: typing.List[str]) -> typing.Dict[str, typing.Union[typing.Dict[str, typing.Dict[str, str]], None]]:
+        raise NotImplementedError
+
+
+class MultiClassAsKeyValuePairDataset(ClassificationAsKeyValuePairDatasetBase):
+    CLASS_NAME_KEY = "className"
+    SCHEMA_BASE = {
+        "name": "Multiclass image classification",
+        "description": "Classify images into one of the provided classes.",
+        "fieldSchema": {
+            f"{CLASS_NAME_KEY}": {
+                "type": "string",
+                "description": "Class name that the image belongs to.",
+                "classes": {}
+            }
+        }
+    }
+
+    def construct_schema(self, class_names: typing.List[str]) -> typing.Dict[str, typing.Any]:
+        schema: typing.Dict[str, typing.Any] = MultiClassAsKeyValuePairDataset.SCHEMA_BASE  # initialize with base schema
+        schema["fieldSchema"][f"{MultiClassAsKeyValuePairDataset.CLASS_NAME_KEY}"]["classes"] = {c: {"description": f"A single class name. Only output {c} as the class name if present."} for c in class_names}
         return schema
 
-    def construct_kvp_label_data(self, label_name: str) -> typing.Dict[str, typing.Union[typing.Dict[str, typing.Dict[str, str]], None]]:
+    def construct_kvp_label_data(self, label_names: typing.List[str]) -> typing.Dict[str, typing.Union[typing.Dict[str, typing.Dict[str, str]], None]]:
         """
-        Convert the classification dataset label_name to the desired format for KVP annnotation as defined by the BASE_CLASSIFICATION_SCHEMA.
+        Convert the classification dataset label_name to the desired format for KVP annnotation as defined by the SCHEMA_BASE.
         E.g. {"fields": {"className": {"value": <label_name>}}}
 
         """
         return {
             f"{KeyValuePairLabelManifest.LABEL_KEY}": {
-                f"{CLASS_NAME_KEY}": {
-                    f"{KeyValuePairLabelManifest.LABEL_VALUE_KEY}": label_name
+                f"{MultiClassAsKeyValuePairDataset.CLASS_NAME_KEY}": {
+                    f"{KeyValuePairLabelManifest.LABEL_VALUE_KEY}": label_names[0]
+                }
+            }
+        }
+
+
+
+class MultiLabelAsKeyValuePairDataset(ClassificationAsKeyValuePairDatasetBase):
+    CLASS_NAME_KEY = "classNames"
+    SCHEMA_BASE = {
+        "name": "Multilabel image classification",
+        "description": "Classify images into one or more of the provided classes.",
+        "fieldSchema": {
+            f"{CLASS_NAME_KEY}": {
+                "type": "array",
+                "items": {
+                    "type": "string",
+                    "description": "Class names that the image belongs to.",
+                    "classes": {}
+                }
+            }
+        }
+    }
+
+    def construct_schema(self, class_names: typing.List[str]) -> typing.Dict[str, typing.Any]:
+        schema: typing.Dict[str, typing.Any] = MultiLabelAsKeyValuePairDataset.SCHEMA_BASE  # initialize with base schema
+        schema["fieldSchema"][f"{MultiLabelAsKeyValuePairDataset.CLASS_NAME_KEY}"]['items']["classes"] = {c: {"description": f"A single class name. Only output {c} as the class name if present."} for c in class_names}
+        return schema
+
+    def construct_kvp_label_data(self, label_names: typing.List[str]) -> typing.Dict[str, typing.Union[typing.Dict[str, typing.Dict[str, str]], None]]:
+        """
+        Convert the classification dataset label_name to the desired format for KVP annnotation as defined by the SCHEMA_BASE.
+        E.g. {"fields": {"className": {"value": <label_name>}}}
+
+        """
+        return {
+            f"{KeyValuePairLabelManifest.LABEL_KEY}": {
+                f"{MultiLabelAsKeyValuePairDataset.CLASS_NAME_KEY}": {
+                    f"{KeyValuePairLabelManifest.LABEL_VALUE_KEY}": label_names
                 }
             }
         }
